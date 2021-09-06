454d59f6d14291738f2243e2ee895c0f4f3cd483
EtshD1 <H79Pain@Gmail.com>
2021-09-06 11:59:55 +0200

    Logout page

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(
+    private _auth: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this._auth.logout();
+    }, 1000);
+  }
+
+}
